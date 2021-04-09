1a33230d69f7df24cf163786e60e3a1ccb6eba12
Jordan <jordanrheeder@gmail.com>
2021-04-09 12:21:11 +0200

    added error boundaries and error handling

---
+++ build/precache-manifest.dfc43ac120828bf52a0451d92e028e6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c296e59aaac6de40614dbe41445cf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e4b0bc7e73a7dc00f7",
+    "url": "/static/css/main.4ecbd5e7.chunk.css"
+  },
+  {
+    "revision": "3594b79133718dd1e40b",
+    "url": "/static/js/2.8af95110.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8af95110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e4b0bc7e73a7dc00f7",
+    "url": "/static/js/main.607e5a63.chunk.js"
+  },
+  {
+    "revision": "150ed36e387c082d59de",
+    "url": "/static/js/runtime-main.f762ba71.js"
+  }
+]);
