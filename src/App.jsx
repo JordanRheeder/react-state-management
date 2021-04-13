3bcc9ac76eb64862d5abc9d7eebebe6ad45a2a4f
Jordan <jordanrheeder@gmail.com>
2021-04-13 15:05:43 +0200

    cleaned up for reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useReducer, useEffect} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -7,41 +7,21 @@ import { Routes, Route } from 'react-router-dom';
 import Detail from "./Detail";
 import Cart from "./Cart";
 import Checkout from "./Checkout";
+import CartReducer from "./CartReducer";
+
+let initialCart;
+
+try {
+  initialCart = JSON.parse(localStorage.getItem('cart')) ?? [];
+} catch (e) {
+console.error("The cart couldn't be parsed into JSON :" + e )
+initialCart = [];
+}
 
 export default function App() {
-  const [cart, setCart] = useState(()=> {
-    try {
-      return JSON.parse(localStorage.getItem('cart')) ?? [];
-    } catch (e) {
-    console.error("The cart couldn't be parsed into JSON :" + e )
-    return [];
-    }
-  });
+  const [cart, dispatch] = useReducer(CartReducer, initialCart);
 
   useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart])
-
-  function addToCart(id, sku) {
-    setCart((items) => {
-      const itemInCart = items.find((i) => i.sku === sku);
-      if (itemInCart) {
-        // return new array with the matching item replaced
-        return items.map((i) => i.sku === sku ? {...i, quantity: i.quantity+ 1} : i);
-      } else {
-        // return new array with the new item appended
-        return [...items, { id, sku, quantity: 1}];
-      }
-    });
-  }
-
-  function updateQuantity(sku, quantity) {
-    setCart((items) => {
-      if (quantity === 0) {
-        return items.filter((i) => i.sku !== sku);
-      }
-      return items.map((i) => i.sku === sku ? {...i, quantity} : i);
-    })
-  }
-
   return (
     <>
       <div className="content">
@@ -50,9 +30,9 @@ export default function App() {
           <Routes>
           <Route path="/" element={<h1>Welcome to the online shop</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route path="/:category/:id" element={<Detail  addToCart={addToCart} />} />
-            <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>} />
-            <Route path="/checkout" element={<Checkout cart={cart} />} />
+            <Route path="/:category/:id" element={<Detail  dispatch={dispatch} />} />
+            <Route path="/cart" element={<Cart cart={cart} dispatch={dispatch}/>} />
+            <Route path="/checkout" element={<Checkout cart={cart} dispatch={dispatch} />} />
           </Routes>
         </main>
       </div>
